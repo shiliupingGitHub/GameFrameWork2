8fb66da770801959cda6e9912bbe944addf08a0c
shiliupingGitHub <582441842@qq.com>
2025-09-10 16:29:26 +0800

    1

---
+++ Assets/Game/Entry/Entry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Game.Common;
+using UnityEngine;
+
+namespace Game.Entry
+{
+    public class Entry : MonoBehaviour
+    {
+        void Start()
+        {
+            new GameInstance().Load();
+        }
+    }
+}
