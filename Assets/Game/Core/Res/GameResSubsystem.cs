56d253700a8d705805079e017eced20dcaf39d88
shiliupingGitHub <582441842@qq.com>
2025-09-12 10:39:53 +0800

    1

---
+++ Assets/Game/Core/Res/GameResSubsystem.cs
@@ -0,0 +1,12 @@
+﻿using Game.Core.Common;
+
+namespace Game.Core.Res
+{
+    public class GameResSubsystem : GameSubsystemWithInstance<GameResSubsystem>
+    {
+        public T LoadAssetSync<T>(string path) where T : UnityEngine.Object
+        {
+            return null;
+        }
+    }
+}
